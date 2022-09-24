3a1a066c17d3c68cf43d02fff7218333be0a1338
pequito193 <vitorspequito@gmail.com>
2022-09-24 10:41:48 +0100

    Updates

---
+++ precache-manifest.302efcccd60581676f15765a559d20b7.js
@@ -0,0 +1,234 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3ac20de28871bdfb928",
+    "url": "/memory-card/static/css/main.b0ce2155.chunk.css"
+  },
+  {
+    "revision": "c3ac20de28871bdfb928",
+    "url": "/memory-card/static/js/main.c3ac20de.chunk.js"
+  },
+  {
+    "revision": "bc9080d7edac7a0a75cf",
+    "url": "/memory-card/static/js/1.bc9080d7.chunk.js"
+  },
+  {
+    "revision": "578ba911c50e8f8ac86f",
+    "url": "/memory-card/static/js/runtime~main.578ba911.js"
+  },
+  {
+    "revision": "c29b28f3eceb7284d141163e3bd37736",
+    "url": "/memory-card/static/media/10_of_clubs.c29b28f3.png"
+  },
+  {
+    "revision": "4324d71291ce16ef4161df8cf852298e",
+    "url": "/memory-card/static/media/10_of_diamonds.4324d712.png"
+  },
+  {
+    "revision": "2e6327a66fbf8d05d379d87d56251a1e",
+    "url": "/memory-card/static/media/10_of_hearts.2e6327a6.png"
+  },
+  {
+    "revision": "2401fe3648865f6bc3c01a538c512d7c",
+    "url": "/memory-card/static/media/10_of_spades.2401fe36.png"
+  },
+  {
+    "revision": "c83a7af4125e22d9733f6514295f3d4c",
+    "url": "/memory-card/static/media/2_of_clubs.c83a7af4.png"
+  },
+  {
+    "revision": "0b5b5249b22ada06106be8b73938be9e",
+    "url": "/memory-card/static/media/2_of_diamonds.0b5b5249.png"
+  },
+  {
+    "revision": "f97ad85b1e89af6a88c70205cdea06f6",
+    "url": "/memory-card/static/media/2_of_hearts.f97ad85b.png"
+  },
+  {
+    "revision": "2354275da56e304199f694388a8aaae6",
+    "url": "/memory-card/static/media/2_of_spades.2354275d.png"
+  },
+  {
+    "revision": "23a7a6ac76db02bbde27988e49ac5fca",
+    "url": "/memory-card/static/media/3_of_clubs.23a7a6ac.png"
+  },
+  {
+    "revision": "b529e1c14fef2ffc07c3c4eac94c31f4",
+    "url": "/memory-card/static/media/3_of_diamonds.b529e1c1.png"
+  },
+  {
+    "revision": "1c16fe1052c3a4d1715ee1288209ebe9",
+    "url": "/memory-card/static/media/3_of_hearts.1c16fe10.png"
+  },
+  {
+    "revision": "ad02dc95b434842c3465a7a4dbaca615",
+    "url": "/memory-card/static/media/3_of_spades.ad02dc95.png"
+  },
+  {
+    "revision": "02deab4916f717f0b9478fad476ec40f",
+    "url": "/memory-card/static/media/4_of_clubs.02deab49.png"
+  },
+  {
+    "revision": "58bd0a6383829cbce3747c245c0b204c",
+    "url": "/memory-card/static/media/4_of_diamonds.58bd0a63.png"
+  },
+  {
+    "revision": "5ad913da63724447b7c94d1fb2d293a6",
+    "url": "/memory-card/static/media/4_of_hearts.5ad913da.png"
+  },
+  {
+    "revision": "df185c634b2fee0418bd613524938832",
+    "url": "/memory-card/static/media/4_of_spades.df185c63.png"
+  },
+  {
+    "revision": "3781bf44a82cad8ef837e6ba281427c9",
+    "url": "/memory-card/static/media/5_of_clubs.3781bf44.png"
+  },
+  {
+    "revision": "e27b4b0aadc6a28953f53cfd573e9e2d",
+    "url": "/memory-card/static/media/5_of_diamonds.e27b4b0a.png"
+  },
+  {
+    "revision": "8f6a2068fd6f32c372ea8dab0cde6f40",
+    "url": "/memory-card/static/media/5_of_hearts.8f6a2068.png"
+  },
+  {
+    "revision": "aa8a072015826f2e582a59e9606e0cd3",
+    "url": "/memory-card/static/media/5_of_spades.aa8a0720.png"
+  },
+  {
+    "revision": "9c2fdf6a8916a2b3daea26a7974eed28",
+    "url": "/memory-card/static/media/6_of_clubs.9c2fdf6a.png"
+  },
+  {
+    "revision": "ee5053d458469b151ef1f4503b5ab12f",
+    "url": "/memory-card/static/media/6_of_diamonds.ee5053d4.png"
+  },
+  {
+    "revision": "9cd2258c8c8c175ead46f94800741891",
+    "url": "/memory-card/static/media/6_of_hearts.9cd2258c.png"
+  },
+  {
+    "revision": "5d221b2a958bb6b66b4e57de437c0906",
+    "url": "/memory-card/static/media/6_of_spades.5d221b2a.png"
+  },
+  {
+    "revision": "112be1dfa65edf2cabf9122b2c49eb22",
+    "url": "/memory-card/static/media/7_of_clubs.112be1df.png"
+  },
+  {
+    "revision": "df3e8e93d277f2b73d2c5ddf348c065c",
+    "url": "/memory-card/static/media/7_of_diamonds.df3e8e93.png"
+  },
+  {
+    "revision": "a9afa10fcea89a4227bb4b81f49a35e8",
+    "url": "/memory-card/static/media/7_of_hearts.a9afa10f.png"
+  },
+  {
+    "revision": "c0f5e5f9013f1d24eccd395ab9312766",
+    "url": "/memory-card/static/media/7_of_spades.c0f5e5f9.png"
+  },
+  {
+    "revision": "6ed0b85e676230d360186c3469b08cdf",
+    "url": "/memory-card/static/media/8_of_clubs.6ed0b85e.png"
+  },
+  {
+    "revision": "8afee604213ca296067245ce18458af2",
+    "url": "/memory-card/static/media/8_of_diamonds.8afee604.png"
+  },
+  {
+    "revision": "e6c01b136dca0b2c3c03a115d4ab21e1",
+    "url": "/memory-card/static/media/8_of_hearts.e6c01b13.png"
+  },
+  {
+    "revision": "747667555c7c5d6799ca3545463372b0",
+    "url": "/memory-card/static/media/8_of_spades.74766755.png"
+  },
+  {
+    "revision": "784ea7703fcff1e10745e014d98a24aa",
+    "url": "/memory-card/static/media/9_of_clubs.784ea770.png"
+  },
+  {
+    "revision": "86608eb9bf92b21b9e33a1ffa4c46ccc",
+    "url": "/memory-card/static/media/9_of_diamonds.86608eb9.png"
+  },
+  {
+    "revision": "cdb8fda5b30f3973b1ec7f200c24a18f",
+    "url": "/memory-card/static/media/9_of_hearts.cdb8fda5.png"
+  },
+  {
+    "revision": "e5b29fdec7e761281073496181d31ab9",
+    "url": "/memory-card/static/media/9_of_spades.e5b29fde.png"
+  },
+  {
+    "revision": "07d1c180bead76a0b8cf8e488dfc2755",
+    "url": "/memory-card/static/media/ace_of_clubs.07d1c180.png"
+  },
+  {
+    "revision": "60b16fdaed475d30edab3bc92f4bd3c9",
+    "url": "/memory-card/static/media/ace_of_diamonds.60b16fda.png"
+  },
+  {
+    "revision": "41453bfa387b05e68828f2b0159c19d9",
+    "url": "/memory-card/static/media/ace_of_hearts.41453bfa.png"
+  },
+  {
+    "revision": "3c00788af85d7ac4fcfeb155464ba71c",
+    "url": "/memory-card/static/media/ace_of_spades.3c00788a.png"
+  },
+  {
+    "revision": "d96ecf4f3c246b8c781f82d1bb2bd1dc",
+    "url": "/memory-card/static/media/jack_of_clubs2.d96ecf4f.png"
+  },
+  {
+    "revision": "c936f66eb6ca786d62a2c49876367017",
+    "url": "/memory-card/static/media/jack_of_diamonds2.c936f66e.png"
+  },
+  {
+    "revision": "9be659a7f009932fe8f49a213c22e439",
+    "url": "/memory-card/static/media/jack_of_hearts2.9be659a7.png"
+  },
+  {
+    "revision": "d77ce402c3977e303abeed8ec5bb35a5",
+    "url": "/memory-card/static/media/jack_of_spades2.d77ce402.png"
+  },
+  {
+    "revision": "f0dc748d3d36f265ab670c8ce0332e3d",
+    "url": "/memory-card/static/media/king_of_clubs2.f0dc748d.png"
+  },
+  {
+    "revision": "9588f72f2c9d6898c9af3b20bcde7c2f",
+    "url": "/memory-card/static/media/king_of_diamonds2.9588f72f.png"
+  },
+  {
+    "revision": "e8d050e1412d4866d34f9a41b0c48238",
+    "url": "/memory-card/static/media/king_of_hearts2.e8d050e1.png"
+  },
+  {
+    "revision": "aebc913c5eb0ecd7859218636929c8ae",
+    "url": "/memory-card/static/media/king_of_spades2.aebc913c.png"
+  },
+  {
+    "revision": "fd943099b2abeb6cf01bebb4dc66cbbe",
+    "url": "/memory-card/static/media/queen_of_clubs2.fd943099.png"
+  },
+  {
+    "revision": "f91b8d276604290828523a097fd4af26",
+    "url": "/memory-card/static/media/queen_of_diamonds2.f91b8d27.png"
+  },
+  {
+    "revision": "8065dc2a89d1be1e9908343fb99d82cc",
+    "url": "/memory-card/static/media/queen_of_hearts2.8065dc2a.png"
+  },
+  {
+    "revision": "445df3cf193554d44a50f43d4597c324",
+    "url": "/memory-card/static/media/queen_of_spades2.445df3cf.png"
+  },
+  {
+    "revision": "7519ccd6f7c0e985ed5e5014cb1f4316",
+    "url": "/memory-card/static/media/Caveat-VariableFont_wght.7519ccd6.ttf"
+  },
+  {
+    "revision": "f2c1e9a8303fdff4db9a6309b499c757",
+    "url": "/memory-card/index.html"
+  }
+];
